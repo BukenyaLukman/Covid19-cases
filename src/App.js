7a205daf97454c030bd424f7f614484f61fc19ab
BukenyaLukman <bukenyalukman@gmail.com>
2020-06-05 15:05:04 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name:"Lukman",
+
+    }
+  }
+
+  
+  
+  render(){
+    return(
+      <div className="App">
+        {this.state.name}
+        <button onClick={() => this.setState({name:'Bukenya'})}>Chanage Name</button>
+      </div>
+    )
+  }
 }
 
 export default App;
